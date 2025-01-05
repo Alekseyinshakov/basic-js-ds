07b6e24cb262a4c1fe79b7f6397891cf98ceb528
Alexey <alexeyinshakov992@gmail.com>
2025-01-05 15:55:11 +0300

    100%

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,101 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  
+  // class ListNode {
+  //   constructor(x) {
+  //     this.value = x;
+  //     this.next = null;
+  //   }
+  // }
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.length = 0;
+    }
+    add(value) {
+      if (this.length === 0) {
+        this.head = new ListNode(value)
+      } else {
+        let curr = this.head;
+
+        while (curr.next) {
+          curr = curr.next;
+        }
+
+        curr.next = new ListNode(value)
+      }
+      this.length++;
+    }
+    removeAt(position) {
+      if (position < 0 || position >= this.length) {
+        return null;
+      }
+      let curr = this.head;
+
+      if (position === 0) {
+        this.head = curr.next;
+      } else {
+        let prev = null;
+        let index = 0;
+
+        while (index < position) {
+          prev = curr;
+          curr = curr.next;
+          index++;
+        }
+        prev.next = curr.next;
+      }
+      this.length--;
+      return curr.value;
+    }
+    removeEverywhere(element) {
+      while(this.indexOf(element) !== -1) {
+        this.removeAt( this.indexOf(element) )
+      }
+    }
+    indexOf(element) {
+      let curr = this.head;
+      let index = 0;
+
+      while (curr) {
+        if (curr.value === element) {
+          return index;
+        }
+
+        curr = curr.next;
+        index++;
+      }
+      return -1;
+    }
+  }
+
+  
+
+  let instance = new LinkedList();
+
+  let cu = l;
+  
+  
+  arrrr = []
+  while (cu) {
+    arrrr.push(cu.value)
+    cu = cu.next;
+  }
+
+  console.log(arrrr);
+  
+  arrrr.forEach(num => {
+    instance.add(num)
+  });
+
+
+  
+  instance.removeEverywhere(k)
+  
+  return instance.head;
+  
 }
 
 module.exports = {
